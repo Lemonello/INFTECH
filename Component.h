264b957dbad1deb01c8c834fe4c44ff6274f5967
Lemonello <nikischov.n@yandex.ru>
2024-05-26 11:54:35 +0300

    Delete useless code

--- Component.h
+++ Component.h
@@ -2,28 +2,10 @@
 
 #include<fstream>
 
-namespace streams
-{
-	using std::istream;
-	using std::ostream;
-	using std::ifstream;
-	using std::ofstream;
-}
-
 const int MAX_SIZE = 16;
-
-using namespace streams;
 
 struct Component
 {
 	char maker[MAX_SIZE];
 	int count{}, code{}, price;
-	// Для массива. 0 - элемент занят, 1 - свободен, 
-	//готов к заполнению
-	bool is_empty{ 0 }; 
-
-	//Перегрузка операторов консольного и файлового ввода и вывода
-	friend ofstream&  operator<<(ofstream& stream, Component ob);
-	friend ifstream& operator>>(ifstream& stream, Component& ob);
-
 };
--- sort.h
+++ sort.h
@@ -3,17 +3,6 @@
 #include"Component.h"
 #include"tasks.h"
 
-
-bool inc_comp_maker(Component ob1, Component ob2);
-bool inc_comp_count(Component ob1, Component ob2);
-bool inc_comp_code(Component ob1, Component ob2);
-bool inc_comp_price(Component ob1, Component ob2);
-
-bool dec_comp_maker(Component ob1, Component ob2);
-bool dec_comp_count(Component ob1, Component ob2);
-bool dec_comp_code(Component ob1, Component ob2);
-bool dec_comp_price(Component ob1, Component ob2);
-
 void sort_data(Component* arr, int size_arr);
 void decrease_order(Component* arr, int size_arr, int crit);
 void increase_order(Component* arr, int size_arr, int crit);
